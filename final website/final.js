8ce7ed662b40b6eaa1828153988e8fab63fe9915
Annie <annie-work@anniedembp.myfiosgateway.com>
2021-04-17 00:38:39 -0400

    Update final.js

--- final website/final.js
+++ final website/final.js
@@ -2,8 +2,16 @@ $(document).ready(function() {
     $(".bunny").click(happybunny);
 
     function happybunny() {
-        $(".bunny").css("background-image","url(happy.GIF)");
+        // $(".bunny").css("background-image","url(happy.GIF)");
+        $(".bunny").addClass("happy")
+        setTimeout(changeBack, 2000);
     }
+
+    function changeBack() {
+        // $(".bunny").css("background-image","url(angry.GIF)");
+        $(".bunny").removeClass("happy");
+    }
+
 
     $(window).mousemove(function(e) {
         let cursor = $(".cursor");
@@ -12,4 +20,31 @@ $(document).ready(function() {
             left: e.clientX - cursor.width() / 2
         });
     });
+
+    $(window).mousemove(function(e) {
+        let cursor = $(".bite");
+        cursor.css({
+            top: e.clientY - cursor.height() / 2,
+            left: e.clientX - cursor.width() / 2
+        });
+    });
+
+    $(".bunny").click(biteCarrot);
+
+    function biteCarrot() {
+        $(".bite").addClass("showing");
+        $(".cursor").addClass("hide");
+        setTimeout(changeCursor,1000);
+    }
+
+    function changeCursor() {
+        $(".bite").removeClass("showing");
+        $(".cursor").removeClass("hide");
+    }
+
+    // $(".speech").hover(speechpresent);
+    // function speechpresent() {
+    //     $(".speech").addClass("present");
+    //     setTimeout (speechgone,)
+    // }
 })
